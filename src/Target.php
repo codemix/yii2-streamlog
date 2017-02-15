7c573b560a7ff71c6e13d2af24de1c2e55e2bed8
Nikolay Dobromirov <nikolay.dobromirov@ffwagency.com>
2017-02-15 19:55:36 +0200

    Implemented the two classes.

--- src/Target.php
+++ src/Target.php
@@ -1,34 +1,17 @@
 <?php
 namespace codemix\streamlog;
 
-use yii\log\Target as BaseTarget;
 use yii\base\InvalidConfigException;
 
 /**
  * A log target for streams in URL format.
  */
-class Target extends BaseTarget
+class Target extends CliTarget
 {
     /**
      * @var string the URL to use. See http://php.net/manual/en/wrappers.php for details.
      */
     public $url;
-
-    /**
-     * Already open writable stream to send logs entries to.
-     *
-     * Recommended for use in CLI context for STDOUT and STDERR.
-     * Property 'url' takes precedence.
-     *
-     * @var resource
-     */
-    public $fp = null;
-
-    /**
-     * @var string|null a string that should replace all newline characters in a log message.
-     * Default ist `null` for no replacement.
-     */
-    public $replaceNewline;
 
     /**
      * Validates class configuration.
@@ -38,51 +21,19 @@ class Target extends BaseTarget
      * @throws InvalidConfigException When unable to aquire a writable stream.
      */
     public function init() {
-        // URL overwrites the passed on stream and is being explicitly opened.
-        if (!empty($this->url) && ($this->fp = @fopen($this->url, 'w')) !== false) {
-            return;
+        if (empty($this->url)) {
+            throw new InvalidConfigException("No url configured.");
         }
-
-        // We should have a writable stream passed in.
-        if (!empty($this->fp)
-            && is_resource($this->fp)
-            && ($metadata = stream_get_meta_data($this->fp))
-            && $metadata['mode'] == 'w'
-        ) {
-            return;
+        if (($fp = @fopen($this->url, 'w')) === false) {
+            throw new InvalidConfigException("Unable to append to '{$this->url}'");
         }
-
-        throw new InvalidConfigException("No writable stream aquired! Set `url` or `fp` properties.");
-    }
-
-    /**
-     * Writes a log message to the given target URL.
-     */
-    public function export()
-    {
-        $callback = [$this, 'formatMessage'];
-        $text = implode("\n", array_map($callback, $this->messages)) . "\n";
-        fwrite($this->fp, $text);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function formatMessage($message)
-    {
-        $text = parent::formatMessage($message);
-        if ($this->replaceNewline !== null) {
-            $text = str_replace("\n", $this->replaceNewline, $text);
-        }
-        return $text;
+        $this->setFp($fp);
     }
 
     /**
      * Clean-up streams that we've opened.
      */
     public function __destruct() {
-        if (!empty($this->url) && $this->fp) {
-            fclose($this->fp);
-        }
+        fclose($this->getFp());
     }
 }
